cf2c68e5d26d5998f7498cce2efb4e36a11b4d9d
Sunil Singh <er.techworld@gmail.com>
2025-01-15 18:09:22 +0530

    code optimisation

---
+++ App_Code/DonationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DonationModel
+/// </summary>
+public class DonationModel
+{
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Decimal Amount { get; set; }
+    public string Address { get; set; }
+
+}
--- V1/Administration/DonationsList.aspx.cs
+++ V1/Administration/DonationsList.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.UI.WebControls;
 
@@ -9,12 +10,20 @@ public partial class Administration_DonationsList : System.Web.UI.Page
 
         if (!IsPostBack)
         {
-            BindDonations(nameSearchQuery: null, addressSearchQuery: null, dateSearchQuery: null, amountSearchQuery: null, sortExpression: "Name", sortDirection: "ASC");
+            BindGridView(nameSearchQuery: null, addressSearchQuery: null, dateSearchQuery: null, amountSearchQuery: null, sortExpression: "Name", sortDirection: "ASC");
         }
     }
 
-    private void BindDonations(string nameSearchQuery = null, string addressSearchQuery = null, string dateSearchQuery = null, string amountSearchQuery = null, string sortExpression = "Name", string sortDirection = "ASC")
+    private void BindGridView(string nameSearchQuery = null, string addressSearchQuery = null, string dateSearchQuery = null, string amountSearchQuery = null, string sortExpression = "Name", string sortDirection = "ASC")
     {
+        List<DonationModel> donations = SearchDonation(nameSearchQuery, addressSearchQuery, dateSearchQuery, amountSearchQuery);
+        GridView.DataSource = donations.ToList();
+        GridView.DataBind();
+    }
+
+    private static List<DonationModel> SearchDonation(string nameSearchQuery, string addressSearchQuery, string dateSearchQuery, string amountSearchQuery)
+    {
+        List<DonationModel> donations = new List<DonationModel>();
         using (CrowdReliefDBDataContext dc = new CrowdReliefDBDataContext())
         {
             DateTime parsedDate;
@@ -22,40 +31,43 @@ public partial class Administration_DonationsList : System.Web.UI.Page
             decimal parsedAmount;
             bool isAmountSearch = decimal.TryParse(amountSearchQuery, out parsedAmount);
 
-            var donationsQuery = dc.Donations
-                                   .Where(d => d.IsTest == false &&
-                                               (string.IsNullOrEmpty(nameSearchQuery) || (d.FirstName + " " + d.LastName).Contains(nameSearchQuery)) &&
-                                               (string.IsNullOrEmpty(addressSearchQuery) || dc.Profiles.Where(p => p.UserId == d.UserId).Select(p => p.Address).FirstOrDefault().Contains(addressSearchQuery)) &&
-                                               (string.IsNullOrEmpty(dateSearchQuery) || (isDateSearch && d.CreatedAt.Date == parsedDate.Date)) &&
-                                               (string.IsNullOrEmpty(amountSearchQuery) || (isAmountSearch && d.Amount == parsedAmount)))
-                                   .Select(d => new
-                                   {
-                                       Name = d.FirstName + " " + d.LastName,
-                                       d.CreatedAt,
-                                       d.Amount,
-                                       Address = dc.Profiles.Where(p => p.UserId == d.UserId).Select(p => p.Address).FirstOrDefault()
-                                   });
+            donations = (from d in dc.Donations
+                         join p in dc.Profiles on d.UserId equals p.UserId into profileGroup
+                         from profile in profileGroup.DefaultIfEmpty()
+                         where d.IsTest == false &&
+                               (string.IsNullOrEmpty(nameSearchQuery) || (d.FirstName + " " + d.LastName).Contains(nameSearchQuery)) &&
+                               (string.IsNullOrEmpty(addressSearchQuery) || (profile != null && profile.Address.Contains(addressSearchQuery))) &&
+                               (string.IsNullOrEmpty(dateSearchQuery) || (isDateSearch && d.CreatedAt.Date == parsedDate.Date)) &&
+                               (string.IsNullOrEmpty(amountSearchQuery) || (isAmountSearch && d.Amount == parsedAmount))
+                         select new DonationModel()
+                         {
+                             Name = d.FirstName + " " + d.LastName,
+                             CreatedAt = d.CreatedAt,
+                             Amount = d.Amount,
+                             Address = profile != null ? profile.Address : null
+                         }).OrderBy(x => x.Name).ToList();
 
-            if (sortExpression != null)
-            {
-                switch (sortExpression)
-                {
-                    case "Name":
-                        donationsQuery = sortDirection == "ASC" ? donationsQuery.OrderBy(d => d.Name) : donationsQuery.OrderByDescending(d => d.Name);
-                        break;
-                        // Add other sorting cases as needed
-                }
-            }
-            GridView.DataSource = donationsQuery.ToList();
-            GridView.DataBind();
+            //if (sortExpression != null)
+            //{
+            //    switch (sortExpression)
+            //    {
+            //        case "Name":
+            //            donationsQuery = sortDirection == "ASC" ? donationsQuery.OrderBy(d => d.Name) : donationsQuery.OrderByDescending(d => d.Name);
+            //            break;
+            //            // Add other sorting cases as needed
+            //    }
+            //}
         }
+
+        return donations;
     }
+
     protected void GridView_Sorting(object sender, GridViewSortEventArgs e)
     {
         string sortExpression = e.SortExpression;
         string sortDirection = e.SortDirection == SortDirection.Ascending ? "ASC" : "DESC";
 
-        BindDonations(nameSearchQuery: null, addressSearchQuery: null, dateSearchQuery: null, amountSearchQuery: null, sortExpression: sortExpression, sortDirection: sortDirection);
+        BindGridView(nameSearchQuery: null, addressSearchQuery: null, dateSearchQuery: null, amountSearchQuery: null, sortExpression: sortExpression, sortDirection: sortDirection);
     }
 
 
@@ -66,7 +78,7 @@ public partial class Administration_DonationsList : System.Web.UI.Page
         string dateSearchQuery = SearchByDate.Text.Trim();
         string amountSearchQuery = SearchByAmount.Text.Trim();
 
-        BindDonations(nameSearchQuery, addressSearchQuery, dateSearchQuery, amountSearchQuery);
+        BindGridView(nameSearchQuery, addressSearchQuery, dateSearchQuery, amountSearchQuery);
     }
 
     protected void ClearButton_Click(object sender, EventArgs e)
@@ -75,13 +87,13 @@ public partial class Administration_DonationsList : System.Web.UI.Page
         SearchByAddress.Text = string.Empty;
         SearchByDate.Text = string.Empty;
         SearchByAmount.Text = string.Empty;
-        BindDonations();
+        BindGridView();
     }
 
     protected void GridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
     {
         GridView.PageIndex = e.NewPageIndex;
-        BindDonations(SearchByName.Text.Trim());
+        BindGridView(SearchByName.Text.Trim());
     }
 
     protected void ExportPdfButton_Click(object sender, EventArgs e)
@@ -90,122 +102,69 @@ public partial class Administration_DonationsList : System.Web.UI.Page
         string addressSearchQuery = SearchByAddress.Text.Trim();
         string dateSearchQuery = SearchByDate.Text.Trim();
         string amountSearchQuery = SearchByAmount.Text.Trim();
-
-        using (CrowdReliefDBDataContext dc = new CrowdReliefDBDataContext())
+        List<DonationModel> donations = SearchDonation(nameSearchQuery, addressSearchQuery, dateSearchQuery, amountSearchQuery);
+        using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
         {
-            DateTime parsedDate;
-            bool isDateSearch = DateTime.TryParse(dateSearchQuery, out parsedDate);
+            var pdfDoc = new iTextSharp.text.Document();
+            iTextSharp.text.pdf.PdfWriter.GetInstance(pdfDoc, ms);
+            pdfDoc.Open();
+            pdfDoc.Add(new iTextSharp.text.Paragraph("Donations List"));
 
-            var donations = dc.Donations
-                              .Where(d => d.IsTest == false &&
-                                          (string.IsNullOrEmpty(nameSearchQuery) ||
-                                           (d.FirstName + " " + d.LastName).Contains(nameSearchQuery)) &&
-                                          (string.IsNullOrEmpty(addressSearchQuery) ||
-                                           dc.Profiles.Where(p => p.UserId == d.UserId)
-                                                      .Select(p => p.Address)
-                                                      .FirstOrDefault()
-                                                      .Contains(addressSearchQuery)) &&
-                                          (string.IsNullOrEmpty(dateSearchQuery) ||
-                                           (isDateSearch && d.CreatedAt.Date == parsedDate.Date)) &&
-                                          (string.IsNullOrEmpty(amountSearchQuery) ||
-                                           d.Amount.ToString().Contains(amountSearchQuery)))
-                              .Select(d => new
-                              {
-                                  Name = d.FirstName + " " + d.LastName,
-                                  d.CreatedAt,
-                                  d.Amount,
-                                  Address = dc.Profiles
-                                               .Where(p => p.UserId == d.UserId)
-                                               .Select(p => p.Address)
-                                               .FirstOrDefault()
-                              })
-                              .OrderBy(d => d.Name)
-                              .ToList();
+            var table = new iTextSharp.text.pdf.PdfPTable(4);
+            table.AddCell("Name");
+            table.AddCell("Address");
+            table.AddCell("Date");
+            table.AddCell("Amount");
 
-            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            foreach (var donation in donations)
             {
-                var pdfDoc = new iTextSharp.text.Document();
-                iTextSharp.text.pdf.PdfWriter.GetInstance(pdfDoc, ms);
-                pdfDoc.Open();
-                pdfDoc.Add(new iTextSharp.text.Paragraph("Donations List"));
+                string formattedDate = donation.CreatedAt.ToString("yyyy-MM-dd");
+                string formattedAmount = string.Format("${0:F2}", donation.Amount);
 
-                var table = new iTextSharp.text.pdf.PdfPTable(4);
-                table.AddCell("Name");
-                table.AddCell("Address");
-                table.AddCell("Date");
-                table.AddCell("Amount");
-
-                foreach (var donation in donations)
-                {
-                    string formattedDate = donation.CreatedAt.ToString("yyyy-MM-dd");
-                    string formattedAmount = string.Format("${0:F2}", donation.Amount);
-
-                    table.AddCell(donation.Name);
-                    table.AddCell(donation.Address);
-                    table.AddCell(formattedDate);
-                    table.AddCell(formattedAmount);
-                }
-
-                pdfDoc.Add(table);
-                pdfDoc.Close();
-
-                Response.ContentType = "application/pdf";
-                Response.AddHeader("content-disposition", "attachment;filename=DonationsList.pdf");
-                Response.OutputStream.Write(ms.GetBuffer(), 0, ms.GetBuffer().Length);
-                Response.OutputStream.Flush();
-                Response.End();
+                table.AddCell(donation.Name);
+                table.AddCell(donation.Address);
+                table.AddCell(formattedDate);
+                table.AddCell(formattedAmount);
             }
+
+            pdfDoc.Add(table);
+            pdfDoc.Close();
+
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", "attachment;filename=DonationsList.pdf");
+            Response.OutputStream.Write(ms.GetBuffer(), 0, ms.GetBuffer().Length);
+            Response.OutputStream.Flush();
+            Response.End();
         }
     }
 
     protected void ExportCsvButton_Click(object sender, EventArgs e)
     {
-        string searchQuery = SearchByName.Text.Trim();
-        using (CrowdReliefDBDataContext dc = new CrowdReliefDBDataContext())
+        string nameSearchQuery = SearchByName.Text.Trim();
+        string addressSearchQuery = SearchByAddress.Text.Trim();
+        string dateSearchQuery = SearchByDate.Text.Trim();
+        string amountSearchQuery = SearchByAmount.Text.Trim();
+        List<DonationModel> donations = SearchDonation(nameSearchQuery, addressSearchQuery, dateSearchQuery, amountSearchQuery);
+
+        Response.Clear();
+        Response.ContentType = "text/csv";
+        Response.AddHeader("content-disposition", "attachment;filename=DonationsList.csv");
+
+        using (System.IO.StringWriter sw = new System.IO.StringWriter())
         {
-            var donations = dc.Donations
-                              .Where(d => d.IsTest == false &&
-                                          (string.IsNullOrEmpty(searchQuery) ||
-                                           (d.FirstName + " " + d.LastName).Contains(searchQuery)
-                                          || d.Amount.ToString().Contains(searchQuery) ||
-                                           dc.Profiles
-                                              .Where(p => p.UserId == d.UserId)
-                                              .Select(p => p.Address)
-                                              .FirstOrDefault()
-                                              .Contains(searchQuery)))
-                              .Select(d => new
-                              {
-                                  Name = d.FirstName + " " + d.LastName,
-                                  d.CreatedAt,
-                                  d.Amount,
-                                  Address = dc.Profiles
-                                               .Where(p => p.UserId == d.UserId)
-                                               .Select(p => p.Address)
-                                               .FirstOrDefault()
-                              })
-                              .OrderBy(d => d.Name)
-                              .ToList();
+            sw.WriteLine("Name,Address,Date,Amount");
 
-            Response.Clear();
-            Response.ContentType = "text/csv";
-            Response.AddHeader("content-disposition", "attachment;filename=DonationsList.csv");
-
-            using (System.IO.StringWriter sw = new System.IO.StringWriter())
+            foreach (var donation in donations)
             {
-                sw.WriteLine("Name,Address,Date,Amount");
-
-                foreach (var donation in donations)
-                {
-                    sw.WriteLine(string.Format("\"{0}\",\"{1}\",\"{2}\",\"{3}\"",
-                        donation.Name,
-                        donation.Address,
-                         donation.CreatedAt.ToString("yyyy-MM-dd HH:mm:ss"),
-                          string.Format("${0:F2}", donation.Amount)));
-                }
-
-                Response.Write(sw.ToString());
-                Response.End();
+                sw.WriteLine(string.Format("\"{0}\",\"{1}\",\"{2}\",\"{3}\"",
+                    donation.Name,
+                    donation.Address,
+                     donation.CreatedAt.ToString("yyyy-MM-dd HH:mm:ss"),
+                      string.Format("${0:F2}", donation.Amount)));
             }
+
+            Response.Write(sw.ToString());
+            Response.End();
         }
     }
 }
