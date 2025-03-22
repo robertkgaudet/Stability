1b2eef54ff4bf63a4ee8a1a1b2a45664772ac353
Sunil Singh <er.techworld@gmail.com>
2025-03-22 18:22:46 +0530

    update dashboard  query

--- V1/Administration/DonationsList.aspx.cs
+++ V1/Administration/DonationsList.aspx.cs
@@ -51,7 +51,7 @@ public partial class Administration_DonationsList : System.Web.UI.Page
         bool isAmountSearch = decimal.TryParse(amountSearchQuery, out parsedAmount);
 
         //var donationsQuery = dc.Donations.Where(d => d.IsTest == false && d.TransactionId != null && d.TransactionId != "");
-        var donationsQuery = dc.Donations.Where(d => d.TransactionId != null && d.TransactionId != "");
+        var donationsQuery = dc.Donations.Where(d => d.TransactionId != null && d.TransactionId != "" && d.DonationStatus != null && d.DonationStatus.Equals(CrowdRelief.Tools.TransactionStatus.Succeeded));
 
         if (!string.IsNullOrEmpty(nameSearchQuery))
         {
