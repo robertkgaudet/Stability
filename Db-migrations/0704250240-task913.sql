608e2a9dccc83356920d667b84d8c9d5f3324857
Shallu <shalludogra19@gmail.com>
2025-04-07 16:06:18 +0530

    fisx bug

--- Db-migrations/0704250240-task913.sql
+++ Db-migrations/0704250240-task913.sql
@@ -36,3 +36,62 @@ SELECT TOP (1000)
 FROM [dbo].[Address] a
 LEFT JOIN [dbo].[County] c ON a.CountyId = c.CountyId
 ORDER BY StateComparison;
+
+
+-- Use this only in SSMS or SQLCMD, not SSDT (Visual Studio DB Projects)
+
+-- Separate any logic into batches using GO
+
+GO
+CREATE OR ALTER PROCEDURE [dbo].[GetDisasterLocationsByCountys]
+    @CountyId UNIQUEIDENTIFIER,
+    @PageNumber INT = 1,
+    @PageSize INT = 10
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @Offset INT = (@PageNumber - 1) * @PageSize;
+
+    WITH PaginatedData AS (
+        SELECT 
+            lp.LocationProfileId,
+            lp.Name AS LocationName,
+            COALESCE(a.Address, '') + ' ' + COALESCE(a.Address2, '') + ', ' + 
+            COALESCE(a.City, '') + ', ' + COALESCE(a.State, '') + ' ' + COALESCE(a.Zip, '') + ', ' +
+            c.Name AS FullAddress,
+            a.GooglePlaceId AS GooglePlacesID,
+            COALESCE(lp.Description, 'No Description') AS Description,
+            CASE 
+                WHEN lp.IsOnMap = 1 AND a.Latitude IS NOT NULL AND a.Longitude IS NOT NULL 
+                THEN CAST(a.Latitude AS NVARCHAR(20)) + ', ' + CAST(a.Longitude AS NVARCHAR(20))
+                ELSE 'Not on Map'
+            END AS Coordinates,
+            COALESCE(lp.PointOfContactName, 'Not Provided') AS PointOfContact,
+            COALESCE(lp.PointOfContactPhoneNumber, 'N/A') AS PhoneNumber,
+            COALESCE(lp.PointOfContactEmail, 'N/A') AS Email,
+            ROW_NUMBER() OVER (ORDER BY lp.Name) AS RowNum,
+            COUNT(*) OVER () AS TotalCount
+        FROM LocationProfile lp
+        LEFT JOIN Address a ON lp.AddressId = a.AddressId
+        LEFT JOIN County c ON a.CountyId = c.CountyId
+        WHERE lp.IsActive = 1
+          AND a.CountyId = @CountyId
+          AND a.GooglePlaceId IS NOT NULL
+    )
+    SELECT 
+        LocationProfileId,  
+        LocationName,
+        FullAddress,
+        GooglePlacesID,
+        Description,
+        Coordinates,
+        PointOfContact,
+        PhoneNumber,
+        Email,
+        TotalCount
+    FROM PaginatedData
+    WHERE RowNum > @Offset AND RowNum <= @Offset + @PageSize
+    ORDER BY RowNum;
+END
+GO
