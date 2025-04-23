d5e454b997215dd546390571680722a04500ed9a
admin <admin@DELL>
2025-04-23 18:39:46 +0530

    Add New Change for Get UserName

--- V1/GetUserName.aspx
+++ V1/GetUserName.aspx
@@ -3,46 +3,30 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-        <div class="row">
-            <div class="col-xs-2 col-sm-3 col-lg-4"></div>
-            <div class="col-xs-8 col-sm-6 col-lg-4" style="min-width:320px !important;max-width:320px !important;">
-				<div class="middle-box text-center loginscreen animated fadeInDown">
-					<h1 class="loginLogo">
-						<img class="img-responsive" src="/V1/Images/Logo-Horizontal-cs.png" />
-					</h1>
-					<div class="m-t-sm text-center">
-						<h4>
-						Disaster-Ready Communities
-						</h4>
-					</div>
-					<p>
-						Be the lifeline your community needs.
-					</p>
-				</div>
-				<p style="font-weight:bold">Get your UserName?</p>
-				<div class="m-t justify-content-center" role="form">
-					<div class="form-group">
-						<asp:PasswordRecovery
-								ID="UserNameRecovery"
-								runat="server"
-								HeaderText=""
-								InstructionText=""
-								InstructionTextStyle-CssClass="m-l"
-								TextBoxStyle-CssClass="form-control"
-								TitleTextStyle-CssClass="font-bold"
-								LabelStyle-CssClass="font-bold"
-								BackColor="White"
-								SubmitButtonText="Recover UserName"
-								UserNameLabelText="Email: "
-								OnSendingMail="UserNameRecovery2_SendingMail"
-								SuccessText="A UserName has been emailed to you.">
-							<SubmitButtonStyle CssClass="btn btn-success btn-block full-width m-b"></SubmitButtonStyle>
-						</asp:PasswordRecovery>
-					</div>
-					<a class="btn btn-block btn-info m-t-lg" href="/SignIn">Back To Sign In</a>
-				</div>
+    <div class="row">
+        <div class="col-xs-2 col-sm-3 col-lg-4"></div>
+        <div class="col-xs-8 col-sm-6 col-lg-4" style="min-width:320px !important;max-width:320px !important;">
+            <div class="middle-box text-center loginscreen animated fadeInDown">
+                <h1 class="loginLogo">
+                    <img class="img-responsive" src="/V1/Images/Logo-Horizontal-cs.png" />
+                </h1>
+                <div class="m-t-sm text-center">
+                    <h4>Disaster-Ready Communities</h4>
+                </div>
+                <p>Be the lifeline your community needs.</p>
             </div>
-			</div>
-            <div class="col-xs-2 col-sm-3 col-lg-4"></div>
-</asp:Content>
 
+            <p style="font-weight:bold;margin-left: 60px;">Get your UserName?</p>
+            <div class="m-t justify-content-center">
+                <asp:Label ID="lblMessage" runat="server" ForeColor="Red" />
+                <div class="form-group">
+                    <asp:Label ID="lblEmail" runat="server" Text="Email:" CssClass="font-bold" />
+                    <asp:TextBox ID="txtEmail" runat="server" CssClass="form-control" />
+                </div>
+                <asp:Button ID="btnRecover" runat="server" Text="Recover UserName" CssClass="btn btn-success btn-block full-width m-b" OnClick="btnRecover_Click" />
+                <a class="btn btn-block btn-info m-t-lg" href="/SignIn">Back To Sign In</a>
+            </div>
+        </div>
+        <div class="col-xs-2 col-sm-3 col-lg-4"></div>
+    </div>
+</asp:Content>
--- V1/GetUserName.aspx.cs
+++ V1/GetUserName.aspx.cs
@@ -8,30 +8,62 @@ using System.Net.Mail;
 
 public partial class V1_PasswordReset : System.Web.UI.Page
 {
-	protected void Page_Load(object sender, EventArgs e)
-	{
-        UserNameRecovery.MailDefinition.Subject = "Stability Get UserName";
-        UserNameRecovery.MailDefinition.BodyFileName = "~/V1/PasswordRecovery.html";
-        UserNameRecovery.MailDefinition.IsBodyHtml = true;
-	}
+    protected void Page_Load(object sender, EventArgs e)
+    {
 
-	protected void CancelEmail(Object sender, MailMessageEventArgs e)
-	{
-		//e.Cancel = true;
-	}
+    }
 
-	protected void UserNameRecovery2_SendingMail(object sender, MailMessageEventArgs e)
-	{
-		string emailFrom			= System.Configuration.ConfigurationManager.AppSettings["emailFrom"].ToString();
-		string emailFromDisplayName = System.Configuration.ConfigurationManager.AppSettings["emailFromDisplayName"].ToString();
-		MailAddress fromAddress = new MailAddress(emailFrom, emailFromDisplayName);
+    protected void btnRecover_Click(object sender, EventArgs e)
+    {
+        string email = txtEmail.Text.Trim();
+        string userName = null;
 
-		e.Message.From = fromAddress;
-		SmtpClient smtp = new SmtpClient();
-		//smtp.EnableSsl = true;
-		smtp.Send(e.Message);
+        using (CrowdReliefDBDataContext dc = new CrowdReliefDBDataContext())
+        {
+            var userId = (from m in dc.aspnet_Memberships
+                          where m.Email == email
+                          select m.UserId).FirstOrDefault();
 
-		//Stop the system from sending the original email.
-		e.Cancel = true;
-	}
+            if (userId != Guid.Empty)
+            {
+                userName = (from u in dc.aspnet_Users
+                            where u.UserId == userId
+                            select u.UserName).FirstOrDefault();
+            }
+        }
+
+        if (!string.IsNullOrEmpty(userName))
+        {
+            try
+            {
+                string emailFrom = System.Configuration.ConfigurationManager.AppSettings["emailFrom"].ToString().Trim();
+                string emailFromDisplayName = System.Configuration.ConfigurationManager.AppSettings["emailFromDisplayName"].ToString().Trim();
+                string bodyTemplate = System.IO.File.ReadAllText(Server.MapPath("~/V1/UserNameRecovery.html"));
+                string emailBody = bodyTemplate.Replace("<%UserName%>", userName);
+
+                MailMessage mail = new MailMessage();
+                mail.To.Add(email);
+                mail.Subject = "Your Crowd Relief Username";
+                mail.Body = emailBody;
+                mail.From = new MailAddress(emailFrom, emailFromDisplayName);
+                mail.IsBodyHtml = true;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Send(mail);
+
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+                lblMessage.Text = "A UserName has been emailed to you.";
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Error sending email: " + ex.Message;
+            }
+        }
+        else
+        {
+            lblMessage.ForeColor = System.Drawing.Color.Red;
+            lblMessage.Text = "Email not found. Please try again.";
+        }
+    }
 }
